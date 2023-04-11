952789a1dad09a86db23ece56e1f92520dd87028
Saba Shavidze <sabashavidze@gmail.com>
2023-04-11 14:37:25 +0400

    chore(bin): remove useless things
    removed useless things for the repository user is creating

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import chalk from 'chalk';
 import { execSync } from 'child_process';
+import fs from 'fs';
 import { createSpinner } from 'nanospinner';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const filename = fileURLToPath(import.meta.url); // ensuring a closs-platform valid absolut path string
+const dirname = path.dirname(filename); // get directory name of filename
+
+const repoName = process.argv[2] || 'create-vite-react-ts-app';
 
 const error = chalk.bold.red;
 const warning = chalk.hex('#FFA500'); // Orange color
@@ -18,16 +26,46 @@ const executeCommand = (command) => {
   return true;
 };
 
-const repoName = process.argv[2];
+const updatePackageJSON = () => {
+  console.log(dirname);
+  const packageJSON = JSON.parse(
+    fs.readFileSync(path.join(repoName, 'package.json')),
+    'utf-8'
+  );
+  console.log(JSON.stringify(packageJSON, null, 4));
+  delete packageJSON.scripts['semantic-release'];
+  delete packageJSON.dependencies.chalk;
+  delete packageJSON.dependencies.nanospinner;
+  delete packageJSON.devDependencies['semantic-release'];
+  delete packageJSON.devDependencies['cz-conventional-changelog']
+  delete packageJSON.keywords;
+  delete packageJSON.repository;
+  delete packageJSON.bin;
+  delete packageJSON.version;
+  delete packageJSON.config;
+  console.log('updated', JSON.stringify(packageJSON, null, 4));
+
+  fs.writeFileSync(
+    path.join(repoName, 'package.json'),
+    JSON.stringify(packageJSON, null, 2)
+  );
+};
 
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/shavidze/create-vite-react-ts-app ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
+
 const spinner = createSpinner(success(`Loading ... \n`));
+
 console.log(success(`Cloning the repository with the name ${repoName}`));
 spinner.start();
+
 const checkout = executeCommand(gitCheckoutCommand);
+
 if (!checkout) process.exit(-1);
 spinner.success({ text: success('Project successfully cloned') });
+
+updatePackageJSON();
+
 console.log(warning(`Installing dependencies for ${repoName}`));
 const installedDeps = executeCommand(installDepsCommand);
 spinner.start();
@@ -43,3 +81,5 @@ spinner.success({
 //   )
 // );
 console.log(neutral(`cd ${repoName} && npm run dev`));
+fs.rmSync(path.join(repoName, '.github'), { recursive: true });
+fs.rmSync(path.join(repoName, 'bin'), { recursive: true });
