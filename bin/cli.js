8348d614ae1e1b151a0ea86f764dfe2c4674e34b
Saba Shavidze <sabashavidze@gmail.com>
2023-04-11 14:45:50 +0400

    refactor(bin/cli.js): replace console
    replaced console by the shelljs library's commands

--- bin/cli.js
+++ bin/cli.js
@@ -1,49 +1,47 @@
 #!/usr/bin/env node
 /* eslint-disable @typescript-eslint/no-var-requires */
+import path from 'path';
+import fs from 'fs';
+
+import shell from 'shelljs';
 import chalk from 'chalk';
 import { execSync } from 'child_process';
-import fs from 'fs';
 import { createSpinner } from 'nanospinner';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const filename = fileURLToPath(import.meta.url); // ensuring a closs-platform valid absolut path string
-const dirname = path.dirname(filename); // get directory name of filename
 
 const repoName = process.argv[2] || 'create-vite-react-ts-app';
 
+// colors
 const error = chalk.bold.red;
 const warning = chalk.hex('#FFA500'); // Orange color
 const neutral = chalk.yellow;
 const success = chalk.green;
+
 const executeCommand = (command) => {
   try {
     execSync(`${command}`, { stdio: 'inherit' });
   } catch (e) {
-    console.error(error(`Faild to execute ${command}`), e);
+    shell.echo(error(`Faild to execute ${command}`), e);
     return false;
   }
   return true;
 };
 
 const updatePackageJSON = () => {
-  console.log(dirname);
   const packageJSON = JSON.parse(
     fs.readFileSync(path.join(repoName, 'package.json')),
     'utf-8'
   );
-  console.log(JSON.stringify(packageJSON, null, 4));
   delete packageJSON.scripts['semantic-release'];
   delete packageJSON.dependencies.chalk;
+  delete packageJSON.dependencies.shelljs;
   delete packageJSON.dependencies.nanospinner;
   delete packageJSON.devDependencies['semantic-release'];
-  delete packageJSON.devDependencies['cz-conventional-changelog']
+  delete packageJSON.devDependencies['cz-conventional-changelog'];
   delete packageJSON.keywords;
   delete packageJSON.repository;
   delete packageJSON.bin;
   delete packageJSON.version;
   delete packageJSON.config;
-  console.log('updated', JSON.stringify(packageJSON, null, 4));
 
   fs.writeFileSync(
     path.join(repoName, 'package.json'),
@@ -56,7 +54,7 @@ const installDepsCommand = `cd ${repoName} && npm install`;
 
 const spinner = createSpinner(success(`Loading ... \n`));
 
-console.log(success(`Cloning the repository with the name ${repoName}`));
+shell.echo(success(`Cloning the repository with the name ${repoName}`));
 spinner.start();
 
 const checkout = executeCommand(gitCheckoutCommand);
@@ -66,7 +64,7 @@ spinner.success({ text: success('Project successfully cloned') });
 
 updatePackageJSON();
 
-console.log(warning(`Installing dependencies for ${repoName}`));
+shell.echo(warning(`Installing dependencies for ${repoName}`));
 const installedDeps = executeCommand(installDepsCommand);
 spinner.start();
 if (!installedDeps) process.exit(-1);
@@ -75,11 +73,6 @@ spinner.success({
     'Congrats! Now, you can run the following commands to start the application'
   ),
 });
-// console.log(
-//   success(
-//     'Congrats! Now, you can run the following commands to start the application'
-//   )
-// );
-console.log(neutral(`cd ${repoName} && npm run dev`));
+shell.echo(neutral(`cd ${repoName} && npm run dev`));
 fs.rmSync(path.join(repoName, '.github'), { recursive: true });
 fs.rmSync(path.join(repoName, 'bin'), { recursive: true });
